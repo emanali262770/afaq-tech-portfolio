8fd7e5cc788265fb1511d11a9e362f84c2411be9
Ali Gates <hacktech877@gmail.com>
2026-01-24 10:58:58 +0500

    fdwsdef

--- src/components/ContactUsSection/ContactFormSection.jsx
+++ src/components/ContactUsSection/ContactFormSection.jsx
@@ -38,7 +38,7 @@ export default function ContactFormSection() {
   setStatus({ loading: true, success: "", error: "" });
 
   try {
-    const res = await fetch("http://localhost:5000/api/contact", {
+    const res = await fetch( `${process.env.NEXT_PUBLIC_API_URL}/contact`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
